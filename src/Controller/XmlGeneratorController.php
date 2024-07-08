cb7dc5829b7a300f01a206a3e729c7a248ed7bca
ksenia <ksekat01@mail.ru>
2024-07-08 12:42:46 +0300

    Исправила ошибку с загрузкой xml

--- src/Controller/XmlGeneratorController.php
+++ src/Controller/XmlGeneratorController.php
@@ -102,21 +102,24 @@ class XmlGeneratorController extends AbstractController
     {
         $xml = new \SimpleXMLElement('<data/>');
 
-        foreach ($fields as $fieldName => $fieldData) {
-            $value = $fieldData['value'] ?? '';
-            $description = $fieldData['description'] ?? '';
+        foreach ($fields as $complexType => $complexTypeFields) {
+            foreach ($complexTypeFields as $fieldName => $fieldData) {
+                $value = $fieldData['value'] ?? '';
+                $description = $fieldData['description'] ?? '';
 
-            // Create XML element for the field
-            $child = $xml->addChild($fieldName, htmlspecialchars($value));
+                // Create XML element for the field
+                $child = $xml->addChild($fieldName, htmlspecialchars($value));
 
-            // Add description attribute if available
-            if (!empty($description)) {
-                $child->addAttribute('description', htmlspecialchars($description));
+                // Add description attribute if available
+                if (!empty($description)) {
+                    $child->addAttribute('description', htmlspecialchars($description));
+                }
             }
         }
 
         return $xml->asXML();
     }
+
 
     /**
      * @throws \Exception
